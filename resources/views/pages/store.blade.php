a03854fcb21f87b3441ef6b2e27734398b988d97
Bohdan Semesko <18fi.b.semesko@std.npu.edu.ua>
2020-09-26 11:50:47 +0200

    minore

--- resources/views/pages/store.blade.php
+++ resources/views/pages/store.blade.php
@@ -72,60 +72,62 @@
             <!-- /aside Widget -->
 
             <!-- aside Widget -->
-            <div class="aside">
-                <h3 class="aside-title">Brand</h3>
-                <div class="checkbox-filter">
-                    <div class="input-checkbox">
-                        <input type="checkbox" id="brand-1">
-                        <label for="brand-1">
-                            <span></span>
-                            SAMSUNG
-                            <small>(578)</small>
-                        </label>
-                    </div>
-                    <div class="input-checkbox">
-                        <input type="checkbox" id="brand-2">
-                        <label for="brand-2">
-                            <span></span>
-                            LG
-                            <small>(125)</small>
-                        </label>
-                    </div>
-                    <div class="input-checkbox">
-                        <input type="checkbox" id="brand-3">
-                        <label for="brand-3">
-                            <span></span>
-                            SONY
-                            <small>(755)</small>
-                        </label>
-                    </div>
-                    <div class="input-checkbox">
-                        <input type="checkbox" id="brand-4">
-                        <label for="brand-4">
-                            <span></span>
-                            SAMSUNG
-                            <small>(578)</small>
-                        </label>
-                    </div>
-                    <div class="input-checkbox">
-                        <input type="checkbox" id="brand-5">
-                        <label for="brand-5">
-                            <span></span>
-                            LG
-                            <small>(125)</small>
-                        </label>
-                    </div>
-                    <div class="input-checkbox">
-                        <input type="checkbox" id="brand-6">
-                        <label for="brand-6">
-                            <span></span>
-                            SONY
-                            <small>(755)</small>
-                        </label>
-                    </div>
-                </div>
-            </div>
+{{--            <div class="aside">--}}
+{{--                <h3 class="aside-title">Brand</h3>--}}
+{{--                <div class="checkbox-filter">--}}
+{{--                    <div class="input-checkbox">--}}
+{{--                        <input type="checkbox" id="brand-1">--}}
+{{--                        <label for="brand-1">--}}
+{{--                            <span></span>--}}
+{{--                            SAMSUNG--}}
+{{--                            <small>(578)</small>--}}
+{{--                        </label>--}}
+{{--                    </div>--}}
+{{--                    <div class="input-checkbox">--}}
+{{--                        <input type="checkbox" id="brand-2">--}}
+{{--                        <label for="brand-2">--}}
+{{--                            <span></span>--}}
+{{--                            LG--}}
+{{--                            <small>(125)</small>--}}
+{{--                        </label>--}}
+{{--                    </div>--}}
+{{--                    <div class="input-checkbox">--}}
+{{--                        <input type="checkbox" id="brand-3">--}}
+{{--                        <label for="brand-3">--}}
+{{--                            <span></span>--}}
+{{--                            SONY--}}
+{{--                            <small>(755)</small>--}}
+{{--                        </label>--}}
+{{--                    </div>--}}
+{{--                    <div class="input-checkbox">--}}
+{{--                        <input type="checkbox" id="brand-4">--}}
+{{--                        <label for="brand-4">--}}
+{{--                            <span></span>--}}
+{{--                            SAMSUNG--}}
+{{--                            <small>(578)</small>--}}
+{{--                        </label>--}}
+{{--                    </div>--}}
+{{--                    <div class="input-checkbox">--}}
+{{--                        <input type="checkbox" id="brand-5">--}}
+{{--                        <label for="brand-5">--}}
+{{--                            <span></span>--}}
+{{--                            LG--}}
+{{--                            <small>(125)</small>--}}
+{{--                        </label>--}}
+{{--                    </div>--}}
+{{--                    <div class="input-checkbox">--}}
+{{--                        <input type="checkbox" id="brand-6">--}}
+{{--                        <label for="brand-6">--}}
+{{--                            <span></span>--}}
+{{--                            SONY--}}
+{{--                            <small>(755)</small>--}}
+{{--                        </label>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+            <br>
             <button type="submit" class="btn btn-success">Submit Filter</button>
+            <a href="{{ route('store.search') }}"><button type="submit" class="btn btn-info pull-right">Reset Filter</button></a>
         </form>
         <!-- /aside Widget -->
 
